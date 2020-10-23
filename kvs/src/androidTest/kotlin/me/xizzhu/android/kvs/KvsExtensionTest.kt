28414d31f1905c5e6209f3f06959cb3a57fcf887
Xizhi Zhu <xizhi.zhu@gmail.com>
2020-10-22 21:20:26 -0500

    Add extension functions (#3)
    Extension functions to use String as key, and get / set Boolean, Double, Float, Int, Long, and String values.

---
+++ kvs/src/androidTest/kotlin/me/xizzhu/android/kvs/KvsExtensionTest.kt
@@ -0,0 +1,186 @@
+/*
+ * Copyright (C) 2020 Xizhi Zhu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.xizzhu.android.kvs
+
+import java.io.File
+import kotlin.test.*
+
+class KvsExtensionTest {
+    private lateinit var kvs: Kvs
+
+    @BeforeTest
+    fun setup() {
+        kvs = newKvs {
+            dir = getTestDir().apply { mkdirs() }.absolutePath
+        }
+    }
+
+    private fun getTestDir(): File = File(System.getProperty("java.io.tmpdir"), "KvsTest")
+
+    @AfterTest
+    fun tearDown() {
+        kvs.close()
+        getTestDir().deleteRecursively()
+    }
+
+    @Test
+    fun testBasicExtension() {
+        assertNull(kvs["key"])
+
+        kvs["key"] = byteArrayOf(1, 2, 3, 4, 5)
+        assertTrue(byteArrayOf(1, 2, 3, 4, 5).contentEquals(kvs["key"]))
+        assertNull(kvs["non-exist"])
+
+        assertTrue(kvs.remove("key"))
+        assertNull(kvs["key"])
+        assertFalse(kvs.remove("non-exists"))
+        assertNull(kvs["non-exist"])
+
+        assertFalse(kvs.remove("key"))
+    }
+
+    @Test
+    fun testBooleanExtension() {
+        assertNull(kvs["key"])
+        assertFalse(kvs.getBoolean("key"))
+        assertTrue(kvs.getBoolean("key", true))
+
+        kvs["key"] = true
+        assertTrue(kvs.getBoolean("key"))
+
+        kvs["key"] = false
+        assertFalse(kvs.getBoolean("key", true))
+
+        assertTrue(kvs.remove("key"))
+        assertNull(kvs["key"])
+        assertFalse(kvs.getBoolean("key"))
+        assertTrue(kvs.getBoolean("key", true))
+    }
+
+    @Test
+    fun testDoubleExtension() {
+        assertNull(kvs["key"])
+        assertEquals(0, kvs.getDouble("key").compareTo(0.0))
+        assertEquals(0, kvs.getDouble("key", 1989.64).compareTo(1989.64))
+
+        kvs["key"] = 64.1989
+        assertEquals(0, kvs.getDouble("key").compareTo(64.1989))
+
+        kvs["key"] = 89.64
+        assertEquals(0, kvs.getDouble("key").compareTo(89.64))
+
+        assertTrue(kvs.remove("key"))
+        assertNull(kvs["key"])
+        assertEquals(0, kvs.getDouble("key").compareTo(0.0))
+        assertEquals(0, kvs.getDouble("key", 1989.64).compareTo(1989.64))
+    }
+
+    @Test(expected = KvsException::class)
+    fun testDoubleExtension_withException() {
+        kvs["key"] = byteArrayOf(1, 2, 3, 4)
+        kvs.getDouble("key")
+    }
+
+    @Test
+    fun testFloatExtension() {
+        assertNull(kvs["key"])
+        assertEquals(0, kvs.getFloat("key").compareTo(0.0F))
+        assertEquals(0, kvs.getFloat("key", 1989.64F).compareTo(1989.64F))
+
+        kvs["key"] = 64.1989F
+        assertEquals(0, kvs.getFloat("key").compareTo(64.1989F))
+
+        kvs["key"] = 89.64F
+        assertEquals(0, kvs.getFloat("key").compareTo(89.64F))
+
+        assertTrue(kvs.remove("key"))
+        assertNull(kvs["key"])
+        assertEquals(0, kvs.getFloat("key").compareTo(0.0F))
+        assertEquals(0, kvs.getFloat("key", 1989.64F).compareTo(1989.64F))
+    }
+
+    @Test(expected = KvsException::class)
+    fun testFloatExtension_withException() {
+        kvs["key"] = byteArrayOf(1, 2, 3)
+        kvs.getFloat("key")
+    }
+
+    @Test
+    fun testIntExtension() {
+        assertNull(kvs["key"])
+        assertEquals(0, kvs.getInt("key"))
+        assertEquals(8964, kvs.getInt("key", 8964))
+
+        kvs["key"] = 6489
+        assertEquals(6489, kvs.getInt("key"))
+
+        kvs["key"] = 64
+        assertEquals(64, kvs.getInt("key"))
+
+        assertTrue(kvs.remove("key"))
+        assertNull(kvs["key"])
+        assertEquals(0, kvs.getInt("key"))
+        assertEquals(89, kvs.getInt("key", 89))
+    }
+
+    @Test(expected = KvsException::class)
+    fun testIntExtension_withException() {
+        kvs["key"] = byteArrayOf(1, 2, 3)
+        kvs.getInt("key")
+    }
+
+    @Test
+    fun testLongExtension() {
+        assertNull(kvs["key"])
+        assertEquals(0L, kvs.getLong("key"))
+        assertEquals(8964L, kvs.getLong("key", 8964L))
+
+        kvs["key"] = 6489L
+        assertEquals(6489L, kvs.getLong("key"))
+
+        kvs["key"] = 64L
+        assertEquals(64L, kvs.getLong("key"))
+
+        assertTrue(kvs.remove("key"))
+        assertNull(kvs["key"])
+        assertEquals(0L, kvs.getLong("key"))
+        assertEquals(19890604L, kvs.getLong("key", 19890604L))
+    }
+
+    @Test(expected = KvsException::class)
+    fun testLongExtension_withException() {
+        kvs["key"] = byteArrayOf(1, 2, 3, 4)
+        kvs.getLong("key")
+    }
+
+    @Test
+    fun testStringExtension() {
+        assertNull(kvs["key"])
+        assertTrue(kvs.getString("key").isEmpty())
+
+        kvs["key"] = "value"
+        assertEquals("value", kvs.getString("key"))
+        assertNull(kvs["non-exist"])
+
+        assertTrue(kvs.remove("key"))
+        assertNull(kvs["key"])
+        assertFalse(kvs.remove("non-exists"))
+        assertNull(kvs["non-exist"])
+
+        assertFalse(kvs.remove("key"))
+    }
+}
--- kvs/src/main/kotlin/me/xizzhu/android/kvs/Kvs.kt
+++ kvs/src/main/kotlin/me/xizzhu/android/kvs/Kvs.kt
@@ -17,6 +17,7 @@
 package me.xizzhu.android.kvs
 
 import java.io.Closeable
+import java.nio.ByteBuffer
 
 interface Kvs : Closeable {
     /**
@@ -38,3 +39,126 @@ interface Kvs : Closeable {
      */
     fun remove(key: ByteArray): Boolean
 }
+
+/**
+ * Returns the value corresponding to the given [key], or `null` if such a key does not exist.
+ * @throws [KvsException]
+ */
+operator fun Kvs.get(key: String): ByteArray? = get(key.toByteArray())
+
+/**
+ * Associates the specified [value] with the given [key].
+ * @throws [KvsException]
+ */
+operator fun Kvs.set(key: String, value: ByteArray) {
+    set(key.toByteArray(), value)
+}
+
+/**
+ * Returns the boolean value corresponding to the given [key].
+ * @throws [KvsException]
+ */
+fun Kvs.getBoolean(key: String, defValue: Boolean = false): Boolean =
+        get(key)?.first()?.let { it != 0.toByte() } ?: defValue
+
+/**
+ * Associates the specified [value] with the given [key].
+ * @throws [KvsException]
+ */
+operator fun Kvs.set(key: String, value: Boolean) {
+    set(key.toByteArray(), byteArrayOf(if (value) 1 else 0))
+}
+
+/**
+ * Returns the double value corresponding to the given [key].
+ * @throws [KvsException]
+ */
+fun Kvs.getDouble(key: String, defValue: Double = 0.0): Double {
+    val bytes = get(key) ?: return defValue
+    if (bytes.size < 8) throw KvsException("Cannot convert value for '$key' to double")
+    return ByteBuffer.wrap(bytes).double
+}
+
+/**
+ * Associates the specified [value] with the given [key].
+ * @throws [KvsException]
+ */
+operator fun Kvs.set(key: String, value: Double) {
+    set(key.toByteArray(), ByteBuffer.allocate(8).putDouble(value).array())
+}
+
+/**
+ * Returns the float value corresponding to the given [key].
+ * @throws [KvsException]
+ */
+fun Kvs.getFloat(key: String, defValue: Float = 0.0F): Float {
+    val bytes = get(key) ?: return defValue
+    if (bytes.size < 4) throw KvsException("Cannot convert value for '$key' to float")
+    return ByteBuffer.wrap(bytes).float
+}
+
+/**
+ * Associates the specified [value] with the given [key].
+ * @throws [KvsException]
+ */
+operator fun Kvs.set(key: String, value: Float) {
+    set(key.toByteArray(), ByteBuffer.allocate(4).putFloat(value).array())
+}
+
+/**
+ * Returns the int value corresponding to the given [key].
+ * @throws [KvsException]
+ */
+fun Kvs.getInt(key: String, defValue: Int = 0): Int {
+    val bytes = get(key) ?: return defValue
+    if (bytes.size < 4) throw KvsException("Cannot convert value for '$key' to int")
+    return ByteBuffer.wrap(bytes).int
+}
+
+/**
+ * Associates the specified [value] with the given [key].
+ * @throws [KvsException]
+ */
+operator fun Kvs.set(key: String, value: Int) {
+    set(key.toByteArray(), ByteBuffer.allocate(4).putInt(value).array())
+}
+
+/**
+ * Returns the long value corresponding to the given [key].
+ * @throws [KvsException]
+ */
+fun Kvs.getLong(key: String, defValue: Long = 0L): Long {
+    val bytes = get(key) ?: return defValue
+    if (bytes.size < 8) throw KvsException("Cannot convert value for '$key' to long")
+    return ByteBuffer.wrap(bytes).long
+}
+
+/**
+ * Associates the specified [value] with the given [key].
+ * @throws [KvsException]
+ */
+operator fun Kvs.set(key: String, value: Long) {
+    set(key.toByteArray(), ByteBuffer.allocate(8).putLong(value).array())
+}
+
+/**
+ * Returns the string value corresponding to the given [key].
+ * @throws [KvsException]
+ */
+fun Kvs.getString(key: String, defValue: String = ""): String =
+        get(key)?.let { String(it) } ?: defValue
+
+/**
+ * Associates the specified [value] with the given [key].
+ * @throws [KvsException]
+ */
+operator fun Kvs.set(key: String, value: String) {
+    set(key.toByteArray(), value.toByteArray())
+}
+
+/**
+ * Removes the specified [key] and its corresponding value.
+ * @return `true` if the value was removed.
+ * @throws [KvsException]
+ */
+fun Kvs.remove(key: String): Boolean = remove(key.toByteArray())
